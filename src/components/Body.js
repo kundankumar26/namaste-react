7fee62ff14eab1bedfaba2faa522cb1f45206f46
Yadav, Kundan Kumar <KundanKumar.Yadav@Clarivate.com>
2023-01-31 22:55:19 +0530

    Day 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { RestaurantList } from "../constants";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
 
 const filterRestaurants = (searchText, restaurants) => {
     if (searchText.length === 0) return restaurants;
@@ -11,8 +11,23 @@ const filterRestaurants = (searchText, restaurants) => {
 
 const Body = () => {
     const [searchText, setSearchText] = useState("");
-    const [restaurants, setRestaurants] = useState(RestaurantList);
-    return (
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [allRestaurants, setAllRestaurants] = useState([]);
+
+    useEffect(() => {
+        getRestaurants();
+    }, []);
+
+    async function getRestaurants() {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&page_type=DESKTOP_WEB_LISTING');
+        const restaurants = await data.json();
+        setFilteredRestaurants(restaurants?.data?.cards[2]?.data?.data?.cards);
+        setAllRestaurants(restaurants?.data?.cards[2]?.data?.data?.cards);
+    };
+
+    if(!allRestaurants) return null;
+
+    return allRestaurants?.length === 0 ? (<Shimmer/>) : (
         <>
             <div>
                 <input
@@ -21,19 +36,19 @@ const Body = () => {
                     value={searchText}
                     onChange={(e) => {
                         setSearchText(e.target.value);
-                        setRestaurants(filterRestaurants(e.target.value, RestaurantList));
+                        setFilteredRestaurants(filterRestaurants(e.target.value, allRestaurants));
                     }}
                 />
                 <button
                     onClick={(e) => {
-                        setRestaurants(filterRestaurants(searchText, RestaurantList));
+                        setFilteredRestaurants(filterRestaurants(searchText, allRestaurants));
                     }}
                 >
                     Search
                 </button>
             </div>
             <div className="restaurant-list">
-                {restaurants.map((restaurant) => {
+                {filteredRestaurants.map((restaurant) => {
                     return (
                         <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
                     );
